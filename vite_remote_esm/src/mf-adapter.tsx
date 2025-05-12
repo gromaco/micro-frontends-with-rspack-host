35fbaac91e5c2ae9581655971814e605656e45b4
Roman Grynyshyn <roman.grynyshyn@instructure.com>
2025-05-12 15:52:49 +0200

    cleanup

--- vite_remote_esm/src/mf-adapter.tsx
+++ vite_remote_esm/src/mf-adapter.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 const roots = new Map();
 
 export async function render(container: HTMLElement, props: object = {}) {
-  if (!container) throw new Error('No container provided for remote render 111111');
+  if (!container) throw new Error('No container provided for remote render');
 
   const root = ReactDOM.createRoot(container);
   roots.set(container, root);
-  console.log('Calling render for1:', container);
+  console.log('Calling render for:', container);
 
   // This dynamic import enables styles injection and modulepreload optimization for the entrypoint
   const {default: App} = await import('./App');
